f569abb88d787d0013a22d406edb6e98ed76550c
Alexander Mandrikov <mandrikov@pm.me>
2024-05-18 11:39:44 +0700

    step 5

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,5 +1,6 @@
 package hexlet.code.schemas;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapSchema implements Schema<Map<?, ?>> {
@@ -7,6 +8,8 @@ public class MapSchema implements Schema<Map<?, ?>> {
     private boolean checkRequired = false;
 
     private Integer sizeof;
+
+    private HashMap<Object, ?> shape;
 
     public MapSchema required() {
         checkRequired = true;
@@ -18,10 +21,16 @@ public class MapSchema implements Schema<Map<?, ?>> {
         return this;
     }
 
+    public MapSchema shape(Map<?, ?> schemas) {
+        this.shape = new HashMap<>(schemas);
+        return this;
+    }
+
     @Override
     public boolean isValid(Map<?, ?> data) {
         return checkRequired(data)
-            && checkSizeof(data);
+            && checkSizeof(data)
+            && checkShape(data);
     }
 
     private boolean checkRequired(Map<?, ?> data) {
@@ -39,6 +48,34 @@ public class MapSchema implements Schema<Map<?, ?>> {
             return false;
         }
         return data.size() == sizeof;
+    }
+
+    private boolean checkShape(Map<?, ?> data) {
+        if (shape == null) {
+            return true;
+        }
+        if (data == null) {
+            return false;
+        }
+
+        for (var entry : shape.entrySet()) {
+            var dataValue = data.get(entry.getKey());
+
+            @SuppressWarnings("unchecked")
+            var fieldSchema = (Schema<Object>) entry.getValue();
+
+            try {
+                var isFieldValid = fieldSchema.isValid(dataValue);
+
+                if (!isFieldValid) {
+                    return false;
+                }
+            } catch (ClassCastException e) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -62,4 +62,51 @@ class MapSchemaTest {
         assertThat(schema.isValid(map3)).isFalse();
     }
 
+    @Test
+    void testShape() {
+        var stringSchema = new StringSchema();
+        var numberSchema = new NumberSchema();
+        var schemas = new HashMap<String, Schema<?>>();
+        schemas.put("firstName", stringSchema.required());
+        schemas.put("lastName", stringSchema.required().minLength(2));
+        schemas.put("age", numberSchema.required().positive().range(1, 150));
+        schema.shape(schemas);
+
+        var human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+        human1.put("age", 30);
+        assertThat(schema.isValid(human1)).isTrue();
+
+        var human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+        human2.put("age", 30);
+        assertThat(schema.isValid(human2)).isFalse();
+
+        var human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+        human2.put("age", 30);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        var human4 = new HashMap<>();
+        human4.put("firstName", "John");
+        human4.put("lastName", "Smith");
+        human4.put("age", null);
+        assertThat(schema.isValid(human4)).isFalse();
+
+        var human5 = new HashMap<>();
+        human5.put("firstName", "John");
+        human5.put("lastName", "Smith");
+        human5.put("age", 200);
+        assertThat(schema.isValid(human5)).isFalse();
+
+        var human6 = new HashMap<>();
+        human6.put("firstName", "John");
+        human6.put("lastName", "Smith");
+        human6.put("age", "200");
+        assertThat(schema.isValid(human6)).isFalse();
+    }
+
 }
